62d1a9dcf59509a42303eaf1546670d74bfba2aa
bmutahhar <mutahharbinmuzaffar@gmail.com>
2021-07-25 01:20:53 +0500

    Added cors

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var path = require("path");
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "/data/db.json"));
+const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 8000;
+
+server.use(middlewares);
+server.use(cors());
 
 server.use(router);
 server.listen(PORT, () => {
